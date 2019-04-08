318e96e8aed83f27ea3906b63d95e57006b68041
Muhokun <47948657+Muhokun@users.noreply.github.com>
2019-04-08 23:02:09 +0400

    Update recognize_faces_image.py

--- recognize_faces_image.py
+++ recognize_faces_image.py
@@ -2,66 +2,34 @@ import face_recognition
 import argparse
 import pickle
 import cv2
-
-###############################################TERMINAL################################
-ap = argparse.ArgumentParser()
-ap.add_argument("-e", "--encodings", required=True,
-	help="path to serialized db of facial encodings")
-ap.add_argument("-i", "--image", required=True,
-	help="path to input image")
-ap.add_argument("-d", "--detection-method", type=str, default="cnn",
-	help="face detection model to use: either `hog` or `cnn`")
-args = vars(ap.parse_args())
-
-#######################################BOS BOS SEYLER#################################################
+#######################################Variables#################################################
 print("Encoding !!!")
-data = pickle.loads(open(args["encodings"], "rb").read())
-
-
-image = cv2.imread(args["image"])
-rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-
+New = pickle.loads(open(args["encodings"], "rb").read())
+image_01 = cv2.imread(args["image"])
+colour = cv2.cvtColor(image_01, cv2.COLOR_BGR2RGB)
 print("Image Reading")
-boxes = face_recognition.face_locations(rgb,
-	model=args["detection_method"])
-encodings = face_recognition.face_encodings(rgb, boxes)
-
-
-names = []
-
+Image = face_recognition.face_locations(colour,model=args["detection_method"])
+encodings = face_recognition.face_encodings(colour, Image)
+array_name = []
 ######################################FOR LOOP##################################################
 for encoding in encodings:
-
-	matches = face_recognition.compare_faces(data["encodings"],
+	one = face_recognition.compare_faces(New["Downloading"],
 		encoding)
-	name = "Unknown"
-
-
-	if True in matches:
-		
-		matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-		counts = {}
-
-
+	Newname = "Unknown"
+	if True in one:
+		matchedIdxs = [i for (i, j) in enumerate(one) if j]
+		face_sum = {}
 		for i in matchedIdxs:
-			name = data["names"][i]
-			counts[name] = counts.get(name, 0) + 1
-
-	
-		name = max(counts, key=counts.get)
-	
-
-	names.append(name)
-
+			newname = New["names"][i]
+			face_sum[newname] = face_sum.get(newname, 0) + 1
+		newname = max(face_sum, key=face_sum.get)
+	array_name.append(name)
 #######################################MATH#################################################
-for ((top, right, bottom, left), name) in zip(boxes, names):
-he image
-	cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 2)
+for ((top, right, bottom, left), name) in zip(Image, array_name):
+	cv2.rectangle(image_01, (left, top), (right, bottom), (0, 255, 0), 2)
 	y = top - 15 if top - 15 > 15 else top + 15
-	cv2.putText(image, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+	cv2.putText(image_01, newname, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
 		0.75, (0, 255, 0), 2)
-
 ########################################################################################
-cv2.imshow("Image", image)
+cv2.imshow("Image", image_01)
 cv2.waitKey(0)
